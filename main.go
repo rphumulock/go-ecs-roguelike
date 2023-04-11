b3cd2c4808edda0aaa343379da58f27620a5dd2c
Peter Humulock <rphumulock@gmail.com>
2023-04-11 00:03:54 -0400

    Starting project

--- main.go
+++ main.go
@@ -2,4 +2,57 @@ package main
 
 import (
 	_ "image/png"
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
 )
+
+// Game holds all data the entire game will need.
+type Game struct {
+	Map GameMap
+}
+
+// NewGame creates a new Game Object and initializes the data
+func NewGame() *Game {
+	g := &Game{}
+	g.Map = NewGameMap()
+	return g
+}
+
+// Update is called each tic.
+func (g *Game) Update() error {
+	return nil
+}
+
+// Draw is called each draw cycle and is where we will blit.
+func (g *Game) Draw(screen *ebiten.Image) {
+	//Draw the Map
+	gd := NewGameData()
+	level := g.Map.Dungeons[0].Levels[0]
+	for x := 0; x < gd.ScreenWidth; x++ {
+		for y := 0; y < gd.ScreenHeight; y++ {
+			tile := level.Tiles[level.GetIndexFromXY(x, y)]
+			op := &ebiten.DrawImageOptions{}
+			op.GeoM.Translate(float64(tile.PixelX), float64(tile.PixelY))
+			screen.DrawImage(tile.Image, op)
+		}
+	}
+}
+
+// Layout will return the screen dimensions.
+func (g *Game) Layout(w, h int) (int, int) {
+	gd := NewGameData()
+	return gd.TileWidth * gd.ScreenWidth, gd.TileHeight * gd.ScreenHeight
+}
+
+func main() {
+
+	g := NewGame()
+	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
+
+	ebiten.SetWindowTitle("Tower")
+
+	if err := ebiten.RunGame(g); err != nil {
+		log.Fatal(err)
+	}
+}
